ffcf291e36ea8be9bb5df3e5bdb61b9f92e8dd63
thjnmcdonald <t.h.j.n.mcdonald@students.uu.nl>
2022-08-31 11:28:58 +0200

    bash script change

--- training_script/single_task_learning/bash_script_generator.py
+++ training_script/single_task_learning/bash_script_generator.py
@@ -1,6 +1,19 @@
-with open('training_script/single_task_learning/script.bash', 'a') as bash_script:
-    for gnn in ['GCN', 'GraphConv']:
-        for i in range(30):
-            for layers in range(1,5):
-                for nodes in [16, 32, 64]:
-                    bash_script.write(f'python training_singletask_model_5_final_better_reporting.py --gnn {gnn} --num_layers {layers} --num_nodes {nodes} \n')
+def bash_script_generator():
+    with open('training_script/single_task_learning/script.bash', 'a') as bash_script:
+        for gnn in ['GCN', 'GraphConv']:
+            for i in range(30):
+                for layers in range(1,5):
+                    for nodes in [16, 32, 64]:
+                        bash_script.write(f'python training_singletask_model_5_final_better_reporting.py --gnn {gnn} --num_layers {layers} --num_nodes {nodes} \n')
+
+
+def bash_script_generator_2(name):
+    with open(f'training_script/single_task_learning/{str(name)}.bash', 'a') as bash_script:
+        for gnn in ['GCN', 'GraphConv']:
+            for i in range(20):
+                for layers in range(1,5):
+                    for nodes in [16, 32, 64]:
+                        bash_script.write(f'python training_singletask_model_5_final_better_reporting.py --gnn {gnn} --num_layers {layers} --num_nodes {nodes} \n')
+
+
+                
