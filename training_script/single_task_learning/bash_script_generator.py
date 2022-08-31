068287141ef404231a7a18ae995d3b9240270307
thjnmcdonald <thjn.mcdonald@gmail.com>
2022-08-31 14:09:34 +0200

    extra runs

--- training_script/single_task_learning/bash_script_generator.py
+++ training_script/single_task_learning/bash_script_generator.py
@@ -16,4 +16,4 @@ def bash_script_generator_2(name, reps, num_layers, node_list):
                         bash_script.write(f'python training_singletask_model_5_final_better_reporting.py --gnn {gnn} --num_layers {layers} --num_nodes {nodes} \n')
 
 
-bash_script_generator_2('test_script', 20, 5, [16, 32, 64])                
+bash_script_generator_2('15more', 15, 4, [16, 32, 64])                
